db90f3ea679ec657b243ab07a209c254cdcb91a1
Kesha Mae Panaligan <115135050+KeshaMae@users.noreply.github.com>
2022-12-05 19:20:24 +0800

    Add files via upload

---
+++ Return.py
@@ -0,0 +1,55 @@
+import Mangalist
+import dt
+def returnManga():
+    name=input("Enter name of Borrower: ")
+    a="Borrow-"+name+".txt"
+    try:
+        with open(a,"r") as f:
+            lines=f.readlines()
+            lines=[a.strip("$") for a in lines]
+    
+        with open(a,"r") as f:
+            data=f.read()
+            print(data)
+    except:
+        print("The Borrower name is incorrect")
+        returnManga()
+
+    b="Return-"+name+".txt"
+    with open(b,"w+")as f:
+        f.write("                Manga Management System \n")
+        f.write("                   Returned By: "+ name+"\n")
+        f.write("    Date: " + dt.getDate()+"    Time:"+ dt.getTime()+"\n\n")
+        f.write("S.N.\t\tBookname\t\tCost\n")
+
+
+    total=0.0
+    for i in range(3):
+        if Mangalist.manganame[i] in data:
+            with open(b,"a") as f:
+                f.write(str(i+1)+"\t\t"+Mangalist.manganame[i]+"\t\t$"+Mangalist.cost[i]+"\n")
+                Mangalist.quantity[i]=int(Mangalist.quantity[i])+1
+            total+=float(Mangalist.cost[i])
+            
+    print("\t\t\t\t\t\t\t"+"$"+str(total))
+    print("Is the book return date expired?")
+    print("Press Y for Yes and N for No")
+    stat=input()
+    if(stat.upper()=="Y"):
+        print("By how many days was the book returned late?")
+        day=int(input())
+        fine=2*day
+        with open(b,"a")as f:
+            f.write("\t\t\t\t\tFine: $"+ str(fine)+"\n")
+        total=total+fine
+    
+
+
+    print("Final Total: "+ "$"+str(total))
+    with open(b,"a")as f:
+        f.write("\t\t\t\t\tTotal: $"+ str(total))
+    
+        
+    with open("Stock.txt","w+") as f:
+            for i in range(3):
+                f.write(Mangalist.manganame[i]+","+Mangalist.authorname[i]+","+str(Mangalist.quantity[i])+","+"$"+Mangalist.cost[i]+"\n")
