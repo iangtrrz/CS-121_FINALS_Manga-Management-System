0b74643070dee8721e41a56de64b144142be2e9c
Adrian Gutierrez <117073242+iangtrrz@users.noreply.github.com>
2022-12-05 19:17:04 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,37 @@
+import Return
+import Mangalist
+import dt
+import Borrow
+
+def start():
+    while(True):
+        print("------------------------------------------------------")
+        print("        Welcome to the Manga Management System     ")
+        print("------------------------------------------------------")
+        print("Enter 1. To Display Mangas")
+        print("Enter 2. To Borrow Manga")
+        print("Enter 3. To Return Manga")
+        print("Enter 4. To Exit")
+        try:
+            a=int(input("Select a choice from 1-4: "))
+            print()
+            if(a==1):
+                with open("stock.txt","r") as f:
+                    lines=f.read()
+                    print(lines)
+                    print ()
+   
+            elif(a==2):
+                Mangalist.mangalist()
+                Borrow.borrowManga()
+            elif(a==3):
+                Mangalist.mangalist()
+                Return.returnManga()
+            elif(a==4):
+                print("Thank you for using Manga Management System")
+                break
+            else:
+                print("Please enter a valid choice from 1-4")
+        except ValueError:
+            print("Please input as suggested.")
+start()
