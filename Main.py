e6c95be613c5515eac458158934bcf4fc298c2ad
Kesha Mae Panaligan <21-04569@g.batstate-u.edu.ph>
2022-12-12 13:26:58 +0800

    Commit

--- Main.py
+++ Main.py
@@ -3,34 +3,36 @@ import utils.Mangalist as Mangalist
 import utils.dt as dt
 import utils.Borrow as Borrow
 
-def main():
-    while(True):
-        print("------------------------------------------------------")
-        print("        Welcome to the Manga Management System     ")
-        print("------------------------------------------------------")
-        print("Enter 1. To Display Mangas")
-        print("Enter 2. To Borrow Manga")
-        print("Enter 3. To Return Manga")
-        print("Enter 4. To Exit")
-        try:
-            a=int(input("Select a choice from 1-4: "))
-            print()
-            if(a==1):
-                with open("stock.txt","r") as f:
-                    lines=f.read()
-                    print(lines)
-                    print ()
+class main:
+    
+    def main():
+        while(True):
+            print("------------------------------------------------------")
+            print("        Welcome to the Manga Management System     ")
+            print("------------------------------------------------------")
+            print("Enter 1. To Display Mangas")
+            print("Enter 2. To Borrow Manga")
+            print("Enter 3. To Return Manga")
+            print("Enter 4. To Exit")
+            try:
+                a=int(input("Select a choice from 1-4: "))
+                print()
+                if(a==1):
+                    with open("stock.txt","r") as f:
+                        lines=f.read()
+                        print(lines)
+                        print ()
    
-            elif(a==2):
-                Mangalist.mangalist()
-                Borrow.borrowManga()
-            elif(a==3):
-                Mangalist.mangalist()
-                Return.returnManga()
-            elif(a==4):
-                print("Thank you for using Manga Management System")
-                break
-            else:
-                print("Please enter a valid choice from 1-4")
-        except ValueError:
-            print("Please input as suggested.")
+                elif(a==2):
+                    Mangalist.mangalist()
+                    Borrow.borrowManga()
+                elif(a==3):
+                    Mangalist.mangalist()
+                    Return.returnManga()
+                elif(a==4):
+                    print("Thank you for using Manga Management System")
+                    break
+                else:
+                    print("Please enter a valid choice from 1-4")
+            except ValueError:
+                        print("Please input as suggested.")
--- utils/Mangalist.py
+++ utils/Mangalist.py
@@ -1,25 +1,27 @@
-def mangalist():
-    global manganame
-    global authorname
-    global quantity
-    global cost
-    manganame=[]
-    authorname=[]
-    quantity=[]
-    cost=[]
-    with open("stock.txt","r") as f:
+class Manga:
+    
+    def mangalist():
+        global manganame
+        global authorname
+        global quantity
+        global cost
+        manganame=[]
+        authorname=[]
+        quantity=[]
+        cost=[]
+        with open("stock.txt","r") as f:
         
-        lines=f.readlines()
-        lines=[x.strip('\n') for x in lines]
-        for i in range(len(lines)):
-            ind=0
-            for a in lines[i].split(','):
-                if(ind==0):
-                    manganame.append(a)
-                elif(ind==1):
-                    authorname.append(a)
-                elif(ind==2):
-                    quantity.append(a)
-                elif(ind==3):
-                    cost.append(a.strip("$"))
-                ind+=1
+            lines=f.readlines()
+            lines=[x.strip('\n') for x in lines]
+            for i in range(len(lines)):
+                ind=0
+                for a in lines[i].split(','):
+                    if(ind==0):
+                        manganame.append(a)
+                    elif(ind==1):
+                        authorname.append(a)
+                    elif(ind==2):
+                        quantity.append(a)
+                    elif(ind==3):
+                        cost.append(a.strip("$"))
+                    ind+=1
